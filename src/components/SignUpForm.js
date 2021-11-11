b987d065c49767f9d04fd1bb1a68a3af6d832261
zarbabi <z.arbabi4@gmail.com>
2021-11-11 23:01:00 +0330

    added state

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,18 +1,45 @@
+import { useState } from "react";
+
 const SignUpFrom = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = ({target}) => {
+    setUserData({ ...userData, [target.name]: target.value });
+  };
+
   return (
-    <div className="App-sign">
+    <div>
       <form>
         <div className="formControl">
           <label>Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.name}
+            name="name"
+          />
         </div>
         <div className="formControl">
           <label>Email</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.email}
+            name="email"
+          />
         </div>
         <div className="formControl">
           <label>Password</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.password}
+            name="password"
+          />
         </div>
         <button>Submit</button>
       </form>
